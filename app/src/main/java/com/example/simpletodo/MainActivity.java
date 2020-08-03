75a457ce9cd9c85d4cc33868123ce25f6eb31d55
Rennah Weng <rennahweng628@gmail.com>
2020-08-02 23:09:22 -0400

    Implemented persistence by saving todo items in file system

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,12 +53,18 @@ public class MainActivity extends AppCompatActivity {
          */
 
         // Define a model class to use as data source for recyclerview
-        items = new ArrayList<>();
-        items.add("Finish Codepath pre-work for android class");
+        loadItems(); // load from file system to into items
+
+
+        // Mock items:
+        /*
+        // items = new ArrayList<>();
+        items.add("Finish Codepath android pre-work");
         items.add("Revise resume for virtual career fair");
         items.add("Watch AWS practitioner videos on udemy");
         items.add("Update Linkedin profile");
         items.add("Thank Codepaths for this amazing opportunity to jumpstart my career!");
+        */
 
         /*
          * Implement REMOVE actions: remove items using onLongClickListener
@@ -68,6 +80,9 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "An item was removed!",
                         Toast.LENGTH_SHORT).show();
+
+                // update change in items in file system
+                saveItems();
             }
         };
 
@@ -97,13 +112,44 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "New item added successfully!",
                         Toast.LENGTH_SHORT).show();
+
+                // save all added items in file system
+                saveItems();
             }
         });
+    }
 
 
-        /*
-         * Implement persistence: store model changes to file system
-         */
+    /*
+     * Implement persistence: store model changes to file system
+     */
+    private File getDataFile() {
+        return new File( getFilesDir(), "todoItems.txt" );
+        // getFilesDir() - returns directory of this app
+    }
 
+    /*
+     * Load items by reading every line of the todoItems file
+     */
+    private void loadItems() {
+        try {
+            items = new ArrayList<>( FileUtils.readLines(getDataFile(), Charset.defaultCharset()) );
+        } catch (IOException e) {
+            // e.printStackTrace();
+            Log.e("MainActivity", "Error reading items in loadItems()");
+            items = new ArrayList<>();
+        }
+    }
+
+    /*
+     * Save items by writing into the todoItems file
+     */
+    private void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            // e.printStackTrace();
+            Log.e("MainActivity", "Error writing items in saveItems()");
+        }
     }
 }
